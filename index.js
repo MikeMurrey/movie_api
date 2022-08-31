12c6624374fffbcc97e98cf0cc3b0339e6912c37
MikeMurrey <108901311+MikeMurrey@users.noreply.github.com>
2022-08-31 14:44:06 -0600

    Update index.js
    add basic info for top 10 movies, add data logging with Morgan, add access to public folder with express.static, create basic get requests, create  basic error handler

--- index.js
+++ index.js
@@ -1,2 +1,77 @@
-const express = require("express");
+const express = require('express'),
+  morgan = require('morgan');
 const app = express();
+
+let topMovies = [
+  {
+    title: 'The Lord of the Rings',
+    director: 'Peter Jackson'
+  },
+  {
+    title: 'Star Wars',
+    director: 'George Lucas'
+  },
+  {
+    title: 'The Thing',
+    director: 'John Carpenter'
+  },
+  {
+    title: 'Predator',
+    director: 'John McTiernan'
+  },
+  {
+    title: 'John Wick',
+    director: 'Chad Stahelski'
+  },
+  {
+    title: 'The Iron Giant',
+    director: 'Brad Bird'
+  },
+  {
+    title: 'Star Trek',
+    director: 'J. J. Abrams'
+  },
+  {
+    title: 'Pirates of the Caribbean',
+    director: 'Gore Verbinski'
+  },
+  {
+    title: 'No Country for Old Men',
+    director: 'Ethan Coen'
+  },
+  {
+    title: 'Die Hard',
+    director: 'John McTiernan'
+  },
+];
+
+// Provide access to static files
+app.use(express.static('public'));
+
+// Log data with Morgan
+app.use(morgan('common'));
+
+
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to my movie website!');
+});
+
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', {root:__dirname});
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('An error occurred');
+});
+
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
